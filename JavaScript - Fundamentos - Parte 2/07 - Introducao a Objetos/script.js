cad74c17d5273869c92d6cea80f6f2b575f4fbe3
mirandalux <mirandalux@MIRANDA>
2021-11-12 08:57:31 -0300

    array e objetos e suas diferenças

---
+++ JavaScript - Fundamentos - Parte 2/07 - Introducao a Objetos/script.js
@@ -0,0 +1,24 @@
+//exemplo de array
+
+const carro1 = ['Vermelho', 2018, 'BMW', 1589];
+
+//exemplo de objeto
+
+const carro_descricao  = {
+    cor : 'Vermelho',
+    anoFabricao : 2018,
+    tipo : 'BMW',
+    kmRodado : 1589, 
+    grupoFab : ['PNI', 'BR', 'KSM']
+};
+
+//Objetos podem conter qualquer tipo de dados, inclusive arrays.
+
+let myCountry = {
+    country: 'Brazil',
+    capital: 'Brasília',
+    language: 'Portuguese',
+    population: 202,
+    neighbours: ['Venezuela', 'Paraguaí', 'Argentina']
+}
+
