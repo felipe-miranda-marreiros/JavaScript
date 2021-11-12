78e872aa0d58979151e8d7f749e5be8cf4ac7f7f
mirandalux <mirandalux@MIRANDA>
2021-11-12 13:18:59 -0300

    visualizar e adicionar propriedades em um objeto

--- JavaScript - Fundamentos - Parte 2/08 - Dot vs Bracket Notation/script.js
+++ JavaScript - Fundamentos - Parte 2/08 - Dot vs Bracket Notation/script.js
@@ -20,11 +20,32 @@ console.log(descricaoPessoa['sobre' + verNomes]);
 
 //Exemplo de quando não sabemos qual propriedade acessar
 
-const interessadoEm = ('O que você quer saber sobre Felipe? Escolha entre primeiroNome, sobreNome, idade, altura, cor, localizacao');
+// const interessadoEm = ('O que você quer saber sobre Felipe? Escolha entre primeiroNome, sobreNome, idade, altura, cor, localizacao');
 
-if(descricaoPessoa[interessadoEm]){ //se o usuário digitar as propriedades que existem dentro do objeto descricaoPessoa, JavaScript retorná com a informaçao
-    console.log(descricaoPessoa[interessadoEm]);
-} else { //caso contrário, se ele digitar outra coisa que não tenho no objeto, ele ficará com a mensagem else.
-    console.log('Propriedade inválida. Escolha entre primeiroNome, sobreNome, idade, altura, cor, localizacao');
-}
+// if(descricaoPessoa[interessadoEm]){ //se o usuário digitar as propriedades que existem dentro do objeto descricaoPessoa, JavaScript retorná com a informaçao
+//     console.log(descricaoPessoa[interessadoEm]);
+// } else { //caso contrário, se ele digitar outra coisa que não tenho no objeto, ele ficará com a mensagem else.
+//     console.log('Propriedade inválida. Escolha entre primeiroNome, sobreNome, idade, altura, cor, localizacao');
+// }
 
+//podemos também adicionar novas propriedades em um objeto com dot ou bracket notations
+
+descricaoPessoa.profissao = 'programador'; //adicionando com dot
+
+descricaoPessoa['situacao'] = 'solteiro'; //adicionando com bracket
+
+console.log(descricaoPessoa);
+
+//challenge
+
+const nomeS = 'Felipe'
+const felipeAmigos = ['Monark', 'Bolsonaro', 'Ifood'];
+
+const felipeMelhoresAmigos = {
+    amigo1 :'Thiago',
+    amigo2 : 'Amanda'
+};
+
+const challenge = `${descricaoPessoa.primeiroNome} has ${descricaoPessoa.localizacao.length} friends and his best friend is called ${felipeMelhoresAmigos.amigo2}.`;
+
+console.log(challenge);
