0995981b1c3b6efd9003e7fc95089fb5253af464
mirandalux <mirandalux@MIRANDA>
2021-11-13 20:01:32 -0300

    Tipos de Dados

---
+++ Exercícios/01 - Exercícios - JavaScript Fundamentos - Parte 1/02 - Tipos de Dados/script.js
@@ -0,0 +1,16 @@
+/* 
+
+LECTURE: Values and Variables
+
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+
+*/
+
+const country = 'Brazil';
+const continent = 'South America';
+const population = 212.6;
+
+console.log(country, continent, population);
+
