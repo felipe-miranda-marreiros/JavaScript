03c8aa9f7296aa7fa1be428511421a0f1c0b1b82
mirandalux <mirandalux@MIRANDA>
2021-11-13 19:58:53 -0300

    solução

--- Exercícios/01 - Exercícios - JavaScript Fundamentos - Parte 1/01 - Valores e Váriaveis/script.js
+++ Exercícios/01 - Exercícios - JavaScript Fundamentos - Parte 1/01 - Valores e Váriaveis/script.js
@@ -7,3 +7,9 @@ assign their values according to your own country (population in millions)
 2. Log their values to the console
 
 */
+
+const country = 'Brazil';
+const continent = 'South America';
+const population = 212.6;
+
+console.log(country, continent, population);
