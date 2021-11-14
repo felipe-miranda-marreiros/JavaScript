6c7ae303c77f83b3f0cb2a517ecd41ef7affb51b
mirandalux <mirandalux@MIRANDA>
2021-11-14 18:46:07 -0300

    Solução 2

--- Exercícios/01 - Exercícios - JavaScript Fundamentos - Parte 1/08 - Operadores de Igualdade == VS ===/script.js
+++ Exercícios/01 - Exercícios - JavaScript Fundamentos - Parte 1/08 - Operadores de Igualdade == VS ===/script.js
@@ -25,9 +25,9 @@ situation
 
 //Solução
 
-let numNeighbours = prompt('How many neighbour countries does your country have?');
+let numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
 
-if (numNeighbours == 1){
+if (numNeighbours === 1){
     console.log('Only 1 border!');
 }else if (numNeighbours > 1){
     console.log('More than 1 border!');
