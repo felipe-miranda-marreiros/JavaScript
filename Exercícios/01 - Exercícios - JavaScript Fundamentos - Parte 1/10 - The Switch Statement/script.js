18b1cc8b39b67ae003b9766f90ff1b7e585f39a1
mirandalux <mirandalux@MIRANDA>
2021-11-14 19:53:45 -0300

    solução

--- Exercícios/01 - Exercícios - JavaScript Fundamentos - Parte 1/10 - The Switch Statement/script.js
+++ Exercícios/01 - Exercícios - JavaScript Fundamentos - Parte 1/10 - The Switch Statement/script.js
@@ -11,3 +11,24 @@ arabic: '5th most spoken language'
 for all other simply log 'Great language too :D'
 
 */
+
+//Solução
+
+let language = 'english';
+
+switch (language){
+    case 'chinese':
+    case 'mandarin': console.log('MOST number of native speakers!');
+    break;
+    case 'spanish': console.log('2nd place in number of native speakers');
+    break;
+    case 'english': console.log('3rd place');
+    break;
+    case 'hindi': console.log('Number 4');
+    break;
+    case 'arabic': console.log('5th most spoken language');
+    break;
+
+    default: console.log('Great language, too!');
+
+}
