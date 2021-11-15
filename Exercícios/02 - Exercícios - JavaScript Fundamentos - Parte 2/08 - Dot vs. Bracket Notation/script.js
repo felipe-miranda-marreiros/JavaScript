6b069f1242962778ca9d080a2428901224c95aa3
mirandalux <mirandalux@MIRANDA>
2021-11-15 13:15:32 -0300

    Enunciado

---
+++ Exercícios/02 - Exercícios - JavaScript Fundamentos - Parte 2/08 - Dot vs. Bracket Notation/script.js
@@ -0,0 +1,33 @@
+/*
+LECTURE: Introduction to Objects
+
+1. Create an object called 'myCountry' for a country of your choice, containing
+properties 'country', 'capital', 'language', 'population' and
+'neighbours' (an array like we used in previous assignments)
+
+*/
+
+const neighbours = new Array ('Cuba', 'Argentina', 'Venezuela');
+
+const myCountry = {
+    
+    country : 'Brazil',
+    capital : 'Brasília', 
+    language : 'Portuguese',
+    neighbour : neighbours
+
+};
+
+console.log(myCountry);
+
+/*
+
+LECTURE: Dot vs. Bracket Notation
+
+1. Using the object from the previous assignment, log a string like this to the
+console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries
+and a capital called Helsinki.'
+2. Increase the country's population by two million using dot notation, and then
+decrease it by two million using brackets notation.
+
+*/
