2582ab7dfc9cc9a10fd14d0e1414662eeb03c084
mirandalux <mirandalux@MIRANDA>
2021-11-15 20:18:50 -0300

    Solução

--- Exercícios/02 - Exercícios - JavaScript Fundamentos - Parte 2/09 - Objetos e Métodos/script.js
+++ Exercícios/02 - Exercícios - JavaScript Fundamentos - Parte 2/09 - Objetos e Métodos/script.js
@@ -14,4 +14,22 @@ there are. Use the ternary operator to set the property.
 
 */
 
+const neighbours = new Array ('Cuba', 'Argentina', 'Venezuela'); //array
 
+const myCountry = { //objeto
+
+    country : 'Brazil',
+    capital : 'Brasília', 
+    language : 'Portuguese',
+    population : 200,
+    neighbour : neighbours, //array
+
+    //método
+    describe () { //usando this
+
+        this.summary = `${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbour.length} neighbouring countries and a capital called ${this.capital}.`;
+        return this.summary;
+    }
+};
+
+console.log(myCountry.describe());
