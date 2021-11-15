5252ab39c91416515897f1217e97707c27763b97
mirandalux <mirandalux@MIRANDA>
2021-11-15 10:34:44 -0300

    Enunciado

---
+++ Exercícios/02 - Exercícios - JavaScript Fundamentos - Parte 2/07 - Introdução a Objetos/script.js
@@ -0,0 +1,9 @@
+/*
+LECTURE: Introduction to Objects
+
+1. Create an object called 'myCountry' for a country of your choice, containing
+properties 'country', 'capital', 'language', 'population' and
+'neighbours' (an array like we used in previous assignments)
+
+*/
+
