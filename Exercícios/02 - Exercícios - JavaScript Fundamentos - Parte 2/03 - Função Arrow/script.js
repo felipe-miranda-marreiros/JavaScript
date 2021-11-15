4c7c15fc97ae3e27a81612d258893180766af164
mirandalux <mirandalux@MIRANDA>
2021-11-14 21:44:38 -0300

    Solução

--- Exercícios/02 - Exercícios - JavaScript Fundamentos - Parte 2/03 - Função Arrow/script.js
+++ Exercícios/02 - Exercícios - JavaScript Fundamentos - Parte 2/03 - Função Arrow/script.js
@@ -39,3 +39,5 @@ LECTURE: Arrow Functions
 1. Recreate the last assignment, but this time create an arrow function called
 'percentageOfWorld3'
 */
+
+const percentageOfWorld3 = (population) => (population / 7900) * 100;
