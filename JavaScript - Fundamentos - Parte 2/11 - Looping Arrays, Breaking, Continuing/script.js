1d859acb2523403384958f8cd39186cf38fc251f
mirandalux <mirandalux@MIRANDA>
2021-11-16 18:54:19 -0300

    Loop com Arrays

---
+++ JavaScript - Fundamentos - Parte 2/11 - Looping Arrays, Breaking, Continuing/script.js
@@ -0,0 +1,13 @@
+//podemos usar arrays com for loop
+
+const felipeArray = [
+    'Felipe',
+    'Miranda',
+    2021 - 1997,
+    'Front-End',
+    ['Amanda', 'Jéssica','Liana']
+];
+
+for(let i = 0; i < 5; i++){ //começamos com 0 porque arrays são zero-based
+    console.log(felipeArray[i]); //em vez de escrevermos a posição de cada elemento da array, usamos a variável.
+};
