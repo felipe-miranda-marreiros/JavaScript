287f053481d0d19e892359c693faeea5b9c250ae
mirandalux <mirandalux@MIRANDA>
2021-11-16 19:05:27 -0300

    replicando uma array com for loop

--- JavaScript - Fundamentos - Parte 2/11 - Looping Arrays, Breaking, Continuing/script.js
+++ JavaScript - Fundamentos - Parte 2/11 - Looping Arrays, Breaking, Continuing/script.js
@@ -9,9 +9,17 @@ const felipeArray = [
     true
 ];
 
+const replica = []; //deixamos uma array vazia que posteriormente vai receber dados do for loop.
+
 for(let i = 0; i < felipeArray.length; i++){ //começamos com 0 porque arrays são zero-based
     console.log(felipeArray[i], typeof felipeArray[i]); //em vez de escrevermos a posição de cada elemento da array, usamos a variável.
+
+    replica[i] = typeof felipeArray[i]; //usamos a mesma declaração for para isso.
 };
 //em vez de manualmente dizer quando o for loop deve parar, no caso do exemplo ele vai parar quando chegar em 5, podemos simplesmente usar a propriedade length.
 
 //Assim, quando adicionarmos novos elementos a Array, não teremos que modificar o for loop toda vez já que basicamente estamos calculando a quantidade de elementos da Array.
+
+//podemos também duplicar uma array com base em outra array criando uma array vazia com base no felipeArray.
+
+console.log(replica);
