59e0c7220fd159bf1db40863d623b6e028d6167e
mirandalux <mirandalux@MIRANDA>
2021-11-16 18:56:51 -0300

    sincronizando array com for loop

--- JavaScript - Fundamentos - Parte 2/11 - Looping Arrays, Breaking, Continuing/script.js
+++ JavaScript - Fundamentos - Parte 2/11 - Looping Arrays, Breaking, Continuing/script.js
@@ -8,6 +8,9 @@ const felipeArray = [
     ['Amanda', 'Jéssica','Liana']
 ];
 
-for(let i = 0; i < 5; i++){ //começamos com 0 porque arrays são zero-based
+for(let i = 0; i < felipeArray.length; i++){ //começamos com 0 porque arrays são zero-based
     console.log(felipeArray[i]); //em vez de escrevermos a posição de cada elemento da array, usamos a variável.
 };
+//em vez de manualmente dizer quando o for loop deve parar, no caso do exemplo ele vai parar quando chegar em 5, podemos simplesmente usar a propriedade length.
+
+//Assim, quando adicionarmos novos elementos a Array, não teremos que modificar o for loop toda vez já que basicamente estamos calculando a quantidade de elementos da Array.
