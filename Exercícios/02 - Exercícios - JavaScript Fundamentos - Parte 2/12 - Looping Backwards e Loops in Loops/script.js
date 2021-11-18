883e3679d0ab22b7f68af9e9210c679aed946a02
mirandalux <mirandalux@MIRANDA>
2021-11-18 11:16:12 -0300

    Enunciado

---
+++ Exercícios/02 - Exercícios - JavaScript Fundamentos - Parte 2/12 - Looping Backwards e Loops in Loops/script.js
@@ -0,0 +1,12 @@
+/*
+
+LECTURE: Looping Backwards and Loops in Loops
+
+1. Store this array of arrays into a variable called 'listOfNeighbours'
+[['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden',
+'Russia']];
+2. Log only the neighbouring countries to the console, one by one, not the entire
+arrays. Log a string like 'Neighbour: Canada' for each country
+
+*/
+
