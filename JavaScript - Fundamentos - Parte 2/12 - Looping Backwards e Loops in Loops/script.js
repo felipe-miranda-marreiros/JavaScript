a3e9858fa79f260acca25387ebdb0e0a3b4fd9ca
mirandalux <mirandalux@MIRANDA>
2021-11-18 11:13:06 -0300

    loops in loops

--- JavaScript - Fundamentos - Parte 2/12 - Looping Backwards e Loops in Loops/script.js
+++ JavaScript - Fundamentos - Parte 2/12 - Looping Backwards e Loops in Loops/script.js
@@ -13,3 +13,15 @@ const felipe = [
 for(let i = felipe.length - 1; i >= 0; i--){
     console.log(i, felipe[i]);
 };
+
+//looping in loops
+
+for(let exercicio = 1; exercicio < 4; exercicio++){ //este loop inicia uma vez a cada 5 carregamentos de pesos (segundo loop);
+    console.log(`Começando exercicio ${exercicio}`);
+
+    //a cada inicio de exercicio, este loop será executado (primeiro loop)
+    for (let repeticao = 1; repeticao < 6; repeticao++){
+        console.log(`Repetição de carregamento de peso ${repeticao}`);
+    }
+
+}
