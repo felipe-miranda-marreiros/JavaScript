fc3622852dbad07341fba132283ad76f4f15b0ce
mirandalux <mirandalux@MIRANDA>
2021-11-22 10:40:24 -0300

    utilizando if statement como primeira regra do jogo

--- Projetos - Utilizando Document Object Model/Guess My Number!/script.js
+++ Projetos - Utilizando Document Object Model/Guess My Number!/script.js
@@ -3,4 +3,8 @@
 document.querySelector('.check').addEventListener('click', function () {
     
     const guess = Number(document.querySelector('.guess').value);
+    
+    if(!guess){
+        document.querySelector('.message').textContent = 'Insira um número para começar!';
+    };
 })
