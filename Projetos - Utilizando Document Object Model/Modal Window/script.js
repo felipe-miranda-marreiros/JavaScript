d90f2d5144920a1cce7dd586625e3f36015b714b
mirandalux <mirandalux@MIRANDA>
2021-11-24 09:50:27 -0300

    incorporando botões de fechar e abrir modal com funções

--- Projetos - Utilizando Document Object Model/Modal Window/script.js
+++ Projetos - Utilizando Document Object Model/Modal Window/script.js
@@ -6,13 +6,24 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 
+const closeModal = function () {
+
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+const openModal = function() {
+    
+    modal.classList.remove('hidden'); 
+    overlay.classList.remove('hidden');
+
+};
+
 for(let i = 0;  i < btnsOpenModal.length; i++){
     
-    btnsOpenModal[i].addEventListener('click', function () {
-        
-        modal.classList.remove('hidden');
-        
-        overlay.classList.remove('hidden');
-    });
+    btnsOpenModal[i].addEventListener('click', openModal);
     
 };
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
