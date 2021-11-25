2c4afdd4acd86e3c1cd79e905cf3cb59558bbfcb
mirandalux <mirandalux@MIRANDA>
2021-11-25 07:25:12 -0300

    selecionando elementos e resetando pontos

--- Projetos - Utilizando Document Object Model/Pig Game/script.js
+++ Projetos - Utilizando Document Object Model/Pig Game/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+//Selecionando elementos:
+const score0El = document.querySelector('#score--0'); //forma de selecionar um elemento por ID, semelhante ao CSS.
+
+const score1El = document.getElementById('score--1'); //forma de selecionar um elemento ID, mas dessa vez usando getElementById. Nesse processo não precisamos usar "#" para selecionar a classe.
+
+//resetando os pontos dos jogadores para 0
+score0El.textContent = 0;
+score1El.textContent = 0;
