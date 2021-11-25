7cb911117d516d0fda1e53b090facd117126f81d
mirandalux <mirandalux@MIRANDA>
2021-11-25 18:05:53 -0300

    refatorando e implementando armazenamento de pontos

--- Projetos - Utilizando Document Object Model/Pig Game/script.js
+++ Projetos - Utilizando Document Object Model/Pig Game/script.js
@@ -21,6 +21,19 @@ const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
+let playing = true;
+
+const switchPlayer = function () { //fará parte do else.
+
+    //essa parte do else irá mudar para o próximo jogador
+    document.getElementById(`current--${activePlayer}`).textContent = 0; //será mostrado para o usuário que o seus pontos voltaram para zero
+        currentScore = 0; //reseta os pontos quando passar para o próximo player
+        activePlayer = activePlayer === 0 ? 1 : 0; //reatribuíndo valor para trocar, dinamicamente, a vez de cada jogador de acordo com as condições acima.        
+        //aqui será usado a função toggle que irá colocar uma classe em um elemento que não tiver ela, no caso player--active está ativo apenas no player--0. Então, posteriormente, ela também irá para player--1 quando for a vez dele.
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
+};
+
 //resetando os pontos dos jogadores para 0
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -37,6 +50,8 @@ let activePlayer = 0; //aqui será trocado para 0 ou 1 de acordo com o IF-ELSE a
 
 btnRoll.addEventListener('click', function () {
     
+    if (playing) { // se o jogo está rodando, então código abaixo será executado.
+
     // 1 - aqui será gerado um valor aleátorio pro dado;
     const dice = Math.trunc(Math.random() * 6) + 1;
 
@@ -54,14 +69,35 @@ btnRoll.addEventListener('click', function () {
         // current0El.textContent = currentScore;
 
     } else {
-        //essa parte do else irá mudar para o próximo jogador
 
-        document.getElementById(`current--${activePlayer}`).textContent = 0; //será mostrado para o usuário que o seus pontos voltaram para zero
-        currentScore = 0; //reseta os pontos quando passar para o próximo player
-        activePlayer = activePlayer === 0 ? 1 : 0; //reatribuíndo valor para trocar, dinamicamente, a vez de cada jogador de acordo com as condições acima.        
-        //aqui será usado a função toggle que irá colocar uma classe em um elemento que não tiver ela, no caso player--active está ativo apenas no player--0. Então, posteriormente, ela também irá para player--1 quando for a vez dele.
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
+        switchPlayer();
     };
-      
+};   
+});
+
+//sistema que vai guardar os pontos caso o jogador opte por segurar os pontos. Ela será guardar na array acima.
+btnHold.addEventListener('click', function () {
+    
+    if (playing) {
+    //Essa função tem como objetivo:
+    
+
+    // 1 - guardará a pontuação para o jogardor 0 ou 1;
+    score[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = score[activePlayer]; 
+
+    //2 - Verifica se um jogador tem pontuação mair ou igual a 100. Se sim, então o jogo acaba aqui.
+    if (score[activePlayer] >= 100){
+
+        playing = false; //fará que o jogo pare quando um jogador ganhe. O jogo só voltará funcionar quando clicar em Novo Jogo.
+        diceEl.classList.add('hidden'); //dado volta a ficar oculto
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+
+        //3 - Se não, o jogo continua e o próximo jogador faz uma nova partida.
+    } else {
+
+        switchPlayer();
+    };
+};
 });
