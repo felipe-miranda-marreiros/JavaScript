3b6ebfe2a278a660becf1c76e6ba309b18b7ebfd
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2021-12-31 19:30:40 -0300

    hosting e variable environment

--- JavaScript - Behind the Scenes/01 - Scoping/script.js
+++ JavaScript - Behind the Scenes/01 - Scoping/script.js
@@ -41,3 +41,43 @@ function calcAge (birthYear){
 
 const firstName = "Felipe"
 calcAge(1996); 
+
+///////////////// Hoisting////////////
+
+//Variables
+
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+
+var me = 'Felipe';
+let job = 'Developer';
+const year = 1997;
+
+//Functions
+
+// console.log(addDecl(2, 3));
+
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl (a, b) {
+    return a + b;
+}
+
+const addExpr = function (a, b){
+    return a + b;
+}
+
+var addArrow = (a, b) => a + b;
+
+// Exemple
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart () {
+    console.log('All products deleted');
+}
