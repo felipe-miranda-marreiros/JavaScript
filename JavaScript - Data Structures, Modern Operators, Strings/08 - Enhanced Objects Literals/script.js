d27982343fbf0f83be37ac4b074ea74013ba2973
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-01-05 15:42:16 -0300

    enhanced objets literals - funções, objetos, arrays

---
+++ JavaScript - Data Structures, Modern Operators, Strings/08 - Enhanced Objects Literals/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const weekdays = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+const openingHours = {
+  [weekdays[1]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[3]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+// Data needed for a later exercise
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// Data needed for first part of the section
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  openingHours, //objeto dentro de objeto
+
+  //nova forma de declarar função, aqui excluí-se function da sintaxe.
+  orderPast(ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+  orderPizza(mainIngredient, ...otherIgredient) {
+    console.log(mainIngredient);
+    console.log(otherIgredient);
+  },
+};
