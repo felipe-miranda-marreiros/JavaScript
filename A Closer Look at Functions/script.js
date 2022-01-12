29d64869132939af9e1007cfd55c47415a3ab3ce
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-01-12 09:17:16 -0300

    Argumentos: valores vs referencia

--- A Closer Look at Functions/script.js
+++ A Closer Look at Functions/script.js
@@ -22,3 +22,32 @@ createBooking("LH123", 2);
 createBooking("LH123", 5);
 
 createBooking("LH123", undefined, 1000);
+
+//////////////////////////////////////////////////////////////////////////////
+
+const flight = "LH234";
+const felipe = {
+  name: "Felipe Miranda",
+  passport: 24558445266,
+};
+
+const checkIn = function (flightNum, passenger) {
+  (flightNum = "LH999"), (passenger.name = "Mr. " + passenger.name);
+
+  if (passenger.passport === 24558445266) {
+    console.log("Check in");
+  } else {
+    console.log("Wrong passport");
+  }
+};
+
+checkIn(flight, felipe);
+console.log(flight);
+console.log(felipe);
+
+const newPassport = function (person) {
+  console.log((person.passport = Math.trunc(Math.random() * 100000000000)));
+};
+
+newPassport(felipe);
+checkIn(flight, felipe);
