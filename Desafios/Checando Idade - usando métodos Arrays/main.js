aa6d26e7a6b160f9c270f35cf86f068629c21465
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-01-25 00:29:25 -0300

    chaining

--- Desafios/Checando Idade - usando métodos Arrays/main.js
+++ Desafios/Checando Idade - usando métodos Arrays/main.js
@@ -95,3 +95,22 @@ function calcAverageHumanAge(arr) {
 
 calcAverageHumanAge(data5);
 calcAverageHumanAge(data6);
+
+/*
+Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time
+as an arrow function, and using chaining!
+
+Test data:
+
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+
+*/
+
+const arrowAverageAge = (arr) =>
+  arr
+    .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter((age) => age > 18)
+    .reduce((acc, age, arr) => acc + age / arr);
+
+console.log(arrowAverageAge(data5));
