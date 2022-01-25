ca40de12fc78a2dc188130493a2a4bae446a42ec
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-01-24 23:57:35 -0300

    Chaining Methods

--- Working With Arrays/04 - Bankist App/script.js
+++ Working With Arrays/04 - Bankist App/script.js
@@ -80,6 +80,29 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const calcDiplaySummary = function (movements) {
+  const incomes = movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov);
+
+  labelSumIn.textContent = `${incomes}€`;
+
+  const outcomes = movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, mov) => acc + mov);
+
+  labelSumOut.textContent = `${Math.abs(outcomes)}€`;
+
+  const interest = movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * 1.2) / 100)
+    .reduce((acc, int) => acc + int);
+
+  labelSumInterest.textContent = `${interest}€`;
+};
+
+calcDiplaySummary(account1.movements);
+
 //criando userName para os usuários
 const createUsernames = function (accounts) {
   accounts.forEach((acc) => {
