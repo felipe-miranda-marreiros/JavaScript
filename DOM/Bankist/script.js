60b341126661fa7eacb21d93862b0a39de6529e0
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-02-09 23:55:33 -0300

    Event Propagation: Bubbling e Capturing

--- DOM/Bankist/script.js
+++ DOM/Bankist/script.js
@@ -86,3 +86,19 @@ btnScrollTo.addEventListener("click", (event) => {
 
 // //Removendo ela
 // setTimeout(() => h1.removeEventListener("mouseenter", h1Alert), 3000);
+
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+// document.querySelector(".nav__link").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+// document.querySelector(".nav__links").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+// document.querySelector(".nav").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
