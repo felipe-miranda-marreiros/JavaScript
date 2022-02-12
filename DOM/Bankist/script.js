064f0e17a09ffbee16508925350fbf421ab6dea5
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-02-12 11:15:17 -0300

    Lazy Loading Images

--- DOM/Bankist/script.js
+++ DOM/Bankist/script.js
@@ -250,7 +250,7 @@ const revealSection = function (entries, observer) {
     return;
   } else {
     entry.target.classList.remove("section--hidden");
-    observer.unobserver(entry.target);
+    observer.unobserve(entry.target);
   }
 };
 const sectionObserver = new IntersectionObserver(revealSection, {
@@ -264,3 +264,27 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add("section--hidden");
 });
+
+////////////////Lazy loading images
+
+const imgTargets = document.querySelectorAll(["img[data-src]"]);
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    return;
+  } else {
+    entry.target.src = entry.target.dataset.src;
+    entry.target.addEventListener("load", function () {
+      entry.target.classList.remove("lazy-img");
+      observer.unobserve(entry.target);
+    });
+  }
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "-200px",
+});
+imgTargets.forEach((img) => imgObserver.observe(img));
